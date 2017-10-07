8c79d82cf4e79a166e54406efbbd0939bce0e8c6
Shaun McCance <shaunm@gnome.org>
2017-10-07 15:03:40 -0400

    Version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mallard-ducktype',
-    version='0.2',
+    version='0.3',
     description='Parse Ducktype files and convert them to Mallard.',
     packages=['mallard', 'mallard.ducktype'],
     scripts=['bin/ducktype'],
